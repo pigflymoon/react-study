b41b429b6d625c926915d036e4613df67daf74ec
pigflymoon <pplflymoon2015@gmail.com>
2016-10-14 12:04:52 +1300

    * How to find state in Readme

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -12,7 +12,7 @@ var ProductRow = React.createClass({
             this.props.product.name :
             <span style={{color: 'red'}}>
         {this.props.product.name}
-        </span>;
+      </span>;
         return (
             <tr>
                 <td>{name}</td>
@@ -27,12 +27,15 @@ var ProductTable = React.createClass({
         var rows = [];
         var lastCategory = null;
         this.props.products.forEach(function (product) {
+            if (product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.inStockOnly)) {
+                return;
+            }
             if (product.category !== lastCategory) {
                 rows.push(<ProductCategoryRow category={product.category} key={product.category}/>);
             }
             rows.push(<ProductRow product={product} key={product.name}/>);
             lastCategory = product.category;
-        });
+        }.bind(this));
         return (
             <table>
                 <thead>
@@ -51,9 +54,9 @@ var SearchBar = React.createClass({
     render: function () {
         return (
             <form>
-                <input type="text" placeholder="Search..."/>
+                <input type="text" placeholder="Search..." value={this.props.filterText}/>
                 <p>
-                    <input type="checkbox"/>
+                    <input type="checkbox" checked={this.props.inStockOnly}/>
                     {' '}
                     Only show products in stock
                 </p>
@@ -63,11 +66,25 @@ var SearchBar = React.createClass({
 });
 
 var FilterableProductTable = React.createClass({
+    getInitialState: function () {
+        return {
+            filterText: '',
+            inStockOnly: false
+        };
+    },
+
     render: function () {
         return (
             <div>
-                <SearchBar />
-                <ProductTable products={this.props.products}/>
+                <SearchBar
+                    filterText={this.state.filterText}
+                    inStockOnly={this.state.inStockOnly}
+                />
+                <ProductTable
+                    products={this.props.products}
+                    filterText={this.state.filterText}
+                    inStockOnly={this.state.inStockOnly}
+                />
             </div>
         );
     }
